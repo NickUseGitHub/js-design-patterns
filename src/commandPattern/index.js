68f84aa37e8197fb420b717511a3aca622942b9c
Chaiwtun <chaiwtun@scg.com>
2021-05-14 01:04:14 +0700

    [command-pattern] complete Command Pattern

--- src/commandPattern/index.js
+++ src/commandPattern/index.js
@@ -1,11 +1,26 @@
-function VehicleManager({ x }) {
-  this.x = x;
+function VehicleManager(speed) {
+  function Vehicle(speed) {
+    this.speed = speed;
+    this.drive = function drive(msg, msg1) {
+      console.log(`Drive speed: ${this.speed} ${msg} ${msg1}`);
+    };
+  }
 
-  return this;
+  const vehicle = new Vehicle(speed);
+
+  function exec(fnName, ...args) {
+    if (!this[fnName]) {
+      console.log(`No ${fnName}`);
+
+      return;
+    }
+
+    return this[fnName](...args);
+  }
+
+  this.exec = exec.bind(vehicle);
 }
 
 export default function main() {
-  const sarmHlor = new VehicleManager({ x: 20 });
-
-  console.log('sarmHlor', sarmHlor);
+  const sarmHlor = new VehicleManager(20);
 }
